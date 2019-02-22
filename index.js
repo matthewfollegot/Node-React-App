1450cbaf3b69d737352608909af39878fc957676
Matthew Follegot <mfollegot@Matthew-Follegot-MacBook-Pro.local>
2019-02-21 21:18:29 -0800

    setup callback url

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = requrie("passport-google-oauth20").Strategy;
+const keys = require("./config/keys.js");
+
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 /*
 new GoogleStrategy() creates a new instance of the Google Passport Strategy
 passport.use() calls passports attention to a specific service
