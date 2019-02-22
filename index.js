7e6e1979ac1f8c0a3ad86ff6dac9f4fc321efe18
Matthew Follegot <mfollegot@Matthew-Follegot-MacBook-Pro.local>
2019-02-21 21:35:17 -0800

    app redirects user to authenticate with google without callback

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const passport = require("passport");
-const GoogleStrategy = requrie("passport-google-oauth20").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("./config/keys.js");
 
 const app = express();
@@ -21,6 +21,14 @@ passport.use(
 new GoogleStrategy() creates a new instance of the Google Passport Strategy
 passport.use() calls passports attention to a specific service
 */
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    //GoogleStrategy has an internal identifier as 'google'
+    scope: ["profile", "email"]
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
