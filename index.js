3b03e9de6a4bea02a4a1c0161ecd10002fd599d0
Matthew Follegot <mfollegot@Matthew-Follegot-MacBook-Pro.local>
2019-02-21 19:54:41 -0800

    passport is setup

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = requrie("passport-google-oauth20").Strategy;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ pce: "man" });
-});
+passport.use(new GoogleStrategy());
+/*
+new GoogleStrategy() creates a new instance of the Google Passport Strategy
+passport.use() calls passports attention to a specific service
+*/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
