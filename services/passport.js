8577a4f731614b88676ad5cfff6c3ff1f5c78d92
Matthew Follegot <mfollegot@Matthew-Follegot-MacBook-Pro.local>
2019-02-23 00:04:05 -0800

    dealt with duplicate users from the same google account

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,17 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we already have a user with the given profile ID
+          done(null, existingUser);
+        } else {
+          //we don't have user record with this ID, so we'll create one
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
