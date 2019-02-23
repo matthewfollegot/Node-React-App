865b3f0e2bbb77ba30a39775dc3aaf69ecdf6f76
Matthew Follegot <mfollegot@Matthew-Follegot-MacBook-Pro.local>
2019-02-22 23:47:14 -0800

    user model is created & appears on MongoDB Atlas

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose; //using destructuring
 
 const userSchema = new Schema({
+  //creating a schema
   googleId: String
 });
 
-mongoose.model("users", userSchema);
+mongoose.model("users", userSchema); //loading a schema
