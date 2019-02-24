9e518f147012528aa3179ff4705c4f7cb1d619a1
Matthew Follegot <mfollegot@Matthew-Follegot-MacBook-Pro.local>
2019-02-24 15:54:34 -0800

    configured prod env

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "547916691445-pvk9rch9m2j18e9biggdhha4t0qrstn6.apps.googleusercontent.com",
+  googleClientSecret: "IARegZJeFuTZ4wzBA5UYF4TB",
+  mongoURI:
+    "mongodb+srv://mfollegot:hg5e5w27@pale-mail-ndaxt.mongodb.net/test?retryWrites=true",
+  cookieKey: "azxcfrtghnmkioloiuytrew"
+};
