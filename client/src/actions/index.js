635527db29989007485716244b61bf120084af09
Matthew Follegot <mfollego@uwaterloo.ca>
2019-03-01 18:01:37 -0800

    actions, dispatch, reducer & everything else is working together successfully

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { FETCH_USER } from "./types";
 
 // fetchUser is an action creator
-const fetchUser = () => {
+export const fetchUser = () => {
   return function(dispatch) {
     axios
       .get("/api/current_user")
