bdae6acc37a21cee0c2af0302d41793a749b6820
Matthew Follegot <mfollego@uwaterloo.ca>
2019-03-01 22:34:57 -0800

    able to deal with state and update client side dynamically through API calls

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,10 +2,8 @@ import axios from "axios";
 import { FETCH_USER } from "./types";
 
 // fetchUser is an action creator
-export const fetchUser = () => {
-  return function(dispatch) {
-    axios
-      .get("/api/current_user")
-      .then(res => dispatch({ type: fetchUser, payload: res }));
-  };
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get("/api/current_user");
+
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,31 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return "still deciding";
+      case false:
+        return "logged out";
+      default:
+        return "looged in";
+    }
+  }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo">Pale Mail</a>
-          <ul className="right">
-            <li>
-              <a>Sign In with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
+function mapStateToProps({ auth }) {
+  return { auth };
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
