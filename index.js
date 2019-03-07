7a439868082c9cceda3026618291a9e3913baba4
Matthew Follegot <mfollego@uwaterloo.ca>
2019-03-06 20:26:45 -0800

    users can now purchase credits and the user model is updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -13,6 +14,7 @@ mongoose.connect(keys.mongoURI).catch(error => {
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -22,7 +24,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+//authroutes and billingRoutes return a function which we call with the express app object
 require("./routes/authroutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
